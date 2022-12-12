a0475c28600f1761bbc42f8adc213c5b363abcca
Claydson Henrique <claydsonhenriquebs@gmail.com>
2022-12-12 18:23:01 -0300

    movendo pasta

---
+++ JavaScriptES6/testando.js
@@ -0,0 +1,63 @@
+// const encoder = (codificar) => {
+//   return codificar.replace(/a/g , 1).replace(/e/g , 2).replace(/i/g , 3).replace(/o/g , 4).replace(/u/g , 5);
+// }
+
+// console.log(encoder('How are you today?' ))
+
+
+
+
+// function encoder (codificar) {
+//   return  codificar.replace(/a/g , 1).replace(/e/g , 2).replace(/i/g , 3).replace(/o/g , 4).replace(/u/g , 5);}
+// console.log(encoder('parabens'));
+
+
+// function decode (decodificar){
+//   return decodificar.replace(/1/g , 'a').replace(/2/g , 'e').replace(/3/g , 'i').replace(/4/g , 'o').replace(/5/g , 'u');}
+// console.log(decode('p1r1b2ns'));
+
+
+// function techList (array){
+//   let maior = array[0];
+//   for (let index = 1; index < array.length; index +=1){
+//     if (array[index] < maior ){
+//       maior = array[index]
+//     }
+//     return maior;
+//   }
+
+// }
+
+// console.log(techList([1,3,4,5,6,7,8,100,21,35]))
+
+// const catAndMouse = (cat1, cat2, mouse) => {
+//   const gato1 = Math.abs(cat1 - mouse);
+//   const gato2 = Math.abs(cat2 - mouse);
+//   if (gato1 < gato2) {
+//     return 'cat1'
+//   } else if (gato2 < gato1) {
+//     return 'cat2'
+//   } else if (gato1 == gato2) {
+//     return "os gatos trombaram e o rato fugiu "
+//   }
+// }
+
+// console.log(catAndMouse(-1, 4, 1))
+
+
+
+
+// const n = `9`;
+
+// let resultado = 2;
+// for (let index = 0; index <= n; index += 1) {
+//   resultado += index;
+// };
+// console.log(resultado);
+// let numero;
+// let some = 0
+// for (let index = 0; index <= 100; index += 1) {
+//   some += numero;
+// }
+
+
